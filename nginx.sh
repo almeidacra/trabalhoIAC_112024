6ac7a53ebc86154f145937043b9ff0e3e8a96f13
almeidacra <161429326+almeidacra@users.noreply.github.com>
2024-11-28 00:51:47 -0300

    Delete nginx.sh

--- nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum update -y
-amazon-linux-extras enable nginx1
-yum install -y nginx
-systemctl enable nginx
-systemctl start nginx
-echo "<h1>Bem Vindo ao NGINX Server</h1>" > /usr/share/nginx/html/index.html
