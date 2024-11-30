7ca9fa135ffb210eaf57a57f7052815737a6b5e7
almeidacra <161429326+almeidacra@users.noreply.github.com>
2024-11-30 01:23:07 -0300

    Delete scripts directory

--- scripts/lamp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y httpd php php-mysqlnd
-systemctl enable httpd
-systemctl start httpd
-echo "<?php phpinfo(); ?>" > /var/www/html/index.php
--- scripts/nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum update -y
-amazon-linux-extras enable nginx1
-yum install -y nginx
-systemctl enable nginx
-systemctl start nginx
-echo "<h1>Welcome to NGINX Server</h1>" > /usr/share/nginx/html/index.html
